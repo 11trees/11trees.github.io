be3130f1599f053f40329f8b740ef0b9d20ae939
Lan Di <landi@Mac.lan>
2026-05-23 19:40:09 +0800

    Deploy app with GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const AppContent = () => {
   const analytics = useAnalytics();
   
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<NotFound />} />
